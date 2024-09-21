77c96ff36fddc199d4af94277f38cc7f7a6a80e8
ander508 <off24001@byui.edu>
2024-09-22 00:53:39 +0100

    updated

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,27 +1,27 @@
-let resources = [
-  "Dev to Community",
-  "Smashing Magazine",
-  "CSS Trick",
-  "W3 School",
-  "Udemy",
-  "Lorem Picsum Image Placeholder",
-];
-const links = [
-  "https://dev.to/",
-  "https://www.smashingmagazine.com/",
-  "https://css-tricks.com/",
-  "https://www.w3schools.com/",
-  "https://udemy.com",
-  "https://picsum.photos/",
-];
+// let resources = [
+//   "Dev to Community",
+//   "Smashing Magazine",
+//   "CSS Trick",
+//   "W3 School",
+//   "Udemy",
+//   "Lorem Picsum Image Placeholder",
+// ];
+// const links = [
+//   "https://dev.to/",
+//   "https://www.smashingmagazine.com/",
+//   "https://css-tricks.com/",
+//   "https://www.w3schools.com/",
+//   "https://udemy.com",
+//   "https://picsum.photos/",
+// ];
 
-let listItems = "";
+// let listItems = "";
 
-for (const resource in resources) {
-  listItems = `${listItems} <li class="margin"><a href="${links[resource]}">${resources[resource]}</a></li>`;
-}
-let resourcesList = document.querySelector("#dev-resource");
-resourcesList.innerHTML = listItems;
+// for (const resource in resources) {
+//   listItems = `${listItems} <li class="margin"><a href="${links[resource]}">${resources[resource]}</a></li>`;
+// }
+// let resourcesList = document.querySelector("#dev-resource");
+// resourcesList.innerHTML = listItems;
 
 const currentYear = new Date().getFullYear();
 const thisYear = document.querySelector("#currentYear");
@@ -35,42 +35,36 @@ thisYear.innerHTML = currentYear;
 // const seconds = new Date().getSeconds();
 
 const lastModified = document.querySelector("#lastModified");
-// dateTime.innerHTML = `Last Modification: ${day}/${
-//   month + 1
-// }/${year} ${hour}:${minutes}:${seconds}`;
-
 let oLastModif = new Date(document.lastModified);
-
 lastModified.innerHTML = oLastModif.toLocaleString();
-
 const clock = document.querySelector(".clock");
 
-function updateClock() {
-  const now = new Date();
-  const hour = now.getHours();
-  const minute = now.getMinutes();
-  const second = now.getSeconds();
+// function updateClock() {
+//   const now = new Date();
+//   const hour = now.getHours();
+//   const minute = now.getMinutes();
+//   const second = now.getSeconds();
 
-  let hours;
-  let minutes;
-  let seconds;
-  if (hour < 10) {
-    hours = "0" + hour;
-  } else {
-    hours = hour;
-  }
-  if (minute < 10) {
-    minutes = "0" + minute;
-  } else {
-    minutes = minute;
-  }
-  if (second < 10) {
-    seconds = "0" + second;
-  } else {
-    seconds = second;
-  }
-  clock.innerHTML = `Time: ${hour}:${minutes}:${seconds}`;
-}
+//   let hours;
+//   let minutes;
+//   let seconds;
+//   if (hour < 10) {
+//     hours = "0" + hour;
+//   } else {
+//     hours = hour;
+//   }
+//   if (minute < 10) {
+//     minutes = "0" + minute;
+//   } else {
+//     minutes = minute;
+//   }
+//   if (second < 10) {
+//     seconds = "0" + second;
+//   } else {
+//     seconds = second;
+//   }
+//   clock.innerHTML = `Time: ${hour}:${minutes}:${seconds}`;
+// }
 
-updateClock();
-setInterval(updateClock, 1000);
+// updateClock();
+// setInterval(updateClock, 1000);
