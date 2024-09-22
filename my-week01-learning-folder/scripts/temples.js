f3ab1e695515b5c081e0f25c61a9f9368d8e8a04
ander508 <off24001@byui.edu>
2024-09-22 02:23:07 +0100

    updated

--- my-week01-learning-folder/scripts/temples.js
+++
@@ -1,9 +0,0 @@
-let temples = ["Aba", "Lagos", "Abuja"];
-listitems = "";
-
-for (const temple of temples) {
-  listitems = `${listitems}  <li>${temple}</li>`;
-}
-
-const items = document.querySelector(".list");
-items.innerHTML = listitems;
